8bfcf43926ed264bf34e90c3de29f567fdf832e0
NithyaP <nithyapanniyan@gmail.com>
2023-02-27 19:23:51 +0530

    Create checkout.java

---
+++ checkout.java
@@ -0,0 +1,13 @@
+package staticCodeCheck;
+
+public class sdomCheckout {
+	
+	public static String test() {
+		return "testing ";
+	}
+
+	public static void main(String args[]) {
+		 test();
+		int a;
+	}
+}
